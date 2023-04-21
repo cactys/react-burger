87e388e83ea4680554891d1a4020f494098bc89a
cactys <cactys95@yandex.ru>
2023-04-21 14:37:56 +0300

    feat: промежуточный

--- src/components/OrderHistory/OrderHistory.tsx
+++ src/components/OrderHistory/OrderHistory.tsx
@@ -1,12 +1,9 @@
-import { useEffect } from 'react';
 import { IOrderMessage } from '../../services/interfaces';
 import { TIngredientItem } from '../../services/types';
 
 import { OwnerOrderFeedsContainer } from '../OwnerOrderFeedsContainer/OwnerOrderFeedsContainer';
 
 import orderHistoryStyle from './OrderHistory.module.css';
-import { useDispatch } from '../../services/hooks';
-import { userChecked } from '../../services/constants';
 
 const OrderHistory = ({
   orders,
@@ -17,17 +14,6 @@ const OrderHistory = ({
   status: string;
   ingredients: TIngredientItem[];
 }) => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const refreshInterval = setInterval(() => {
-      dispatch(userChecked());
-    }, 60 * 1000);
-    console.log(refreshInterval, ' прошло секунд');
-
-    return () => clearInterval(refreshInterval);
-  }, [setInterval]);
-
   return (
     <ul className={orderHistoryStyle.container}>
       <OwnerOrderFeedsContainer
